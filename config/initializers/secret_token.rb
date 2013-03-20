5cd5311d15cf9606bc59424d3a4f5e6c6c377eb5
Daniel Becker <daniel.becker@kotisdesign.com>
2013-03-19 18:16:41 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cogo::Application.config.secret_token = '8d0847b5d4911074666bb5551af8aab8699312b06daecffa632942596ecf256cc65ce67c2a17879d4e09dc6713feff1815f9f5fab852dd08d9e006b5144b34d8'
