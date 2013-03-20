c5613f2888b9fd48dc8931ddec353ae38176e2da
Daniel Becker <daniel.becker@kotisdesign.com>
2013-03-20 16:31:06 -0700

    Finish temporary pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,75 @@
+class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    if signed_in?
+      redirect_to current_user
+    else
+      @user = User.new
+    end    
+  end
+
+  def create
+  
+    if signed_in?
+      redirect_to root_path
+    else  
+      @user = User.new(params[:user])
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
+  end
+
+  def edit
+    #@user = User.find(params[:id])
+  end
+  
+  def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user == current_user
+      redirect_to root_path
+    else
+      @user.destroy
+      flash[:success] = "User #{params[:id]} destroyed."
+      redirect_to users_url
+    end
+  end
+    
+  private
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Cogo::Application.routes.draw do
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  root to: 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
